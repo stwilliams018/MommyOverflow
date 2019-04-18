75dcef9de6d937e752ad87891da3d1223f14ccf4
ekhan3119 <ekc3119@gmail.com>
2019-04-18 13:56:04 -0400

    created new db connection query  and put theusername and password in the config/keys file, stablished models schema relations. Added proxysetUp.

--- models/event.js
+++ models/event.js
@@ -1,21 +1,13 @@
 const mongoose = require('mongoose');
-const Schema =  mongoose.Schema;
+const Schema = mongoose.Schema;
 
-const eventSchema = new Schema({
-    title: {
-        type:String,
+const questionSchema = new Schema({
+    content: {
+        type: String,
         required: true,
     },
-    description: {
-        type: String,
-        required:true
-    },
-    price: {
-        type:Number,
-        required: true
-    },
     date: {
-        type:Date,
+        type: Date,
         required: true
     },
     creator: {
@@ -24,4 +16,4 @@ const eventSchema = new Schema({
     }
 });
 
-module.exports = mongoose.model('Event', eventSchema);
+module.exports = mongoose.model('Event', questionSchema);
