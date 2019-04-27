7c8be397225d836ed7a12194716133ce2b65f68a
ekhan3119 <ekc3119@gmail.com>
2019-04-27 00:28:51 -0400

    created back to front routes, created test register and login components

--- client/src/components/Navigation/MainNavigation.js
+++ client/src/components/Navigation/MainNavigation.js
@@ -9,27 +9,30 @@ function mainNavigation() {
     <div >
 
 
-    <nav className="navbar is-link" role="navigation" aria-label="main navigation">
-      <div className="navbar-brand">
-        <a className="navbar-item" href="https://bulma.io">
-          <h1 className="title is-2">MommyOverflow</h1>
-        </a>
-      </div>
+      <nav className="navbar is-link" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item" href="https://bulma.io">
+            <h1 className="title is-2">MommyOverflow</h1>
+          </a>
+        </div>
 
-      <div className="navbar-end">
-        <button className="navbar-item">
-          <NavLink className="nav-link" to="/Auth">Login</NavLink>
-        </button>
-        <button className="navbar-item">
-          <NavLink className="nav-link" to="/Home">Home</NavLink>
-        </button>
-        <button className="navbar-item">
-          <NavLink className="nav-link" to="/Profile">Profile</NavLink>
-        </button>
-      </div>
-    </nav>
+        <div className="navbar-end">
+          <button className="navbar-item">
+            <NavLink className="nav-link" to="/login">Login</NavLink>
+          </button>
+          <button className="navbar-item">
+            <NavLink className="nav-link" to="/register">Sign Up</NavLink>
+          </button>
+          <button className="navbar-item">
+            <NavLink className="nav-link" to="/Home">Home</NavLink>
+          </button>
+          <button className="navbar-item">
+            <NavLink className="nav-link" to="/profile">Profile</NavLink>
+          </button>
+        </div>
+      </nav>
     </div>
-    
+
   );
 }
 export default mainNavigation;
